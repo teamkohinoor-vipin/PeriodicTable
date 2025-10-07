4658192b486ed4513f8095a5b566b8e989d54c46
teamkohinoor-vipin <teamkohinoor2@gmail.com>
2025-10-08 02:26:48 +0530

    footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,8 @@ export function Footer() {
   return (
     <footer className="border-t border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-4 text-center text-muted-foreground text-sm z-10">
       <div className="container">
-        <p>© {new Date().getFullYear()} Drbaph All Rights Reserved</p>
+        <p>© {new Date().getFullYear()} All Rights Reserved by Team Kohinoor</p>
+        <p>VIPIN|TEAM KPHINOOR</p>
       </div>
     </footer>
   )
