7e2b145459019836cfa2f71765dbc46f2877663c
teamkohinoor-vipin <teamkohinoor2@gmail.com>
2025-10-08 02:38:29 +0530

    header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import type React from "react"
 
 import Link from "next/link"
 import Image from "next/image"
-import { Github } from "lucide-react"
+import { Telegram } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from "@/components/mode-toggle"
 import { SearchBar } from "@/components/search-bar"
@@ -72,13 +72,13 @@ export function Header({ onHomeClick }: HeaderProps) {
           <ModeToggle />
           <Button variant="ghost" size="icon" asChild>
             <Link
-              href="https://github.com/Saganaki22/PeriodicTable"
+              href="https://t.me/KohinoorOfficial1"
               target="_blank"
               rel="noopener noreferrer"
               title="View on GitHub"
             >
-              <Github className="h-5 w-5" />
-              <span className="sr-only">GitHub</span>
+              <Telegram className="h-5 w-5" />
+              <span className="sr-only">Telegram</span>
             </Link>
           </Button>
         </div>
