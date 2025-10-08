2860e504ccd53f49765264d5db6c8ac5e01a6d55
teamkohinoor-vipin <teamkohinoor2@gmail.com>
2025-10-08 09:14:41 +0530

    header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -75,10 +75,10 @@ export function Header({ onHomeClick }: HeaderProps) {
               href="https://t.me/KohinoorOfficial1"
               target="_blank"
               rel="noopener noreferrer"
-              title="View on GitHub"
+              title="View on Telegram"
             >
-              <Github className="h-5 w-5" />
-              <span className="sr-only">GitHub</span>
+              <Telegram className="h-5 w-5" />
+              <span className="sr-only">Telegram</span>
             </Link>
           </Button>
         </div>
